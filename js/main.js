8ef0db976ca197acc06ed68961e9bbc2b621b9a1
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-05-04 18:58:51 +0200

    bonus iniziali

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": "https://unsplash.it/300/300?image=29"
+            "image": null
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -61,37 +61,76 @@ const container = document.getElementById("container");
 posts.forEach((element) => {
 
     element.created = element.created.split("-").reverse().join("-");
-    container.innerHTML += `
-    <div class="post">
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+
+    if(element.author.image === null){
+        container.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <div class="profile-pic-default">
+                            <span>${getFirstNameLetter(element.author.name)}</span>
+                        </div>                   
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
                 </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${element.author.name}</div>
-                    <div class="post-meta__time">${element.created}</div>
-                </div>                    
             </div>
-        </div>
-        <div class="post__text">${element.content}</div>
-        <div class="post__image">
-            <img src="${element.media}" alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" data-postid="${element.id}">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`;
+    }else{
+        container.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
                 </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
-                </div>
-            </div> 
-        </div>            
-    </div>`;
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`;
+    }
+
+    
 });
 
 const likeButtons = document.querySelectorAll(".like-button");
@@ -122,3 +161,15 @@ function unlike(id){
     likedPosts = likedPosts.filter((element) => element != id);
 }
 
+
+function getFirstNameLetter(string) {
+    string = string.split(" ");
+    let iniziali = "";
+
+    string.forEach((element, index, array) => {
+        array[index] = array[index].split("");
+        array[index] = array[index][0];
+        iniziali += array[index];
+    })
+    return iniziali;
+}
